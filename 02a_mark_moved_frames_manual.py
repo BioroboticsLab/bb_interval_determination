31fdcce584458ef07a6a73e08b343a723358545b
gitmirgut <gitmirgut@users.noreply.github.com>
2017-06-25 23:40:20 +0200

    Rename files/dirs.

--- 02a_mark_moved_frames_manual.py
+++ 02a_mark_moved_frames_manual.py
@@ -107,7 +107,7 @@ class Application(tk.Frame):
         self.go_right()
 
 
-path = 'raw_data/frames_per_day/2016/cam_1'
+path = 'raw_data/02_frames_per_day/2016/cam_1'
 csv_out_path = 'derived_data/02_man_marked_moved_frames/manuel_Cam_1.csv'
 shown = 0
 curr = 0
