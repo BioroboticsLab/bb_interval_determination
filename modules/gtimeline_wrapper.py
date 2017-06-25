21600409dbec2654ad665b43b7269f3ace7c9e77
gitmirgut <gitmirgut@users.noreply.github.com>
2017-06-25 21:06:35 +0200

    pep8

--- modules/gtimeline_wrapper.py
+++ modules/gtimeline_wrapper.py
@@ -1,3 +1,6 @@
+
+
+
 def create_gtimeline_dict(df, row_label, bar_label, start, end, tooltip):
     """Create dict from dataframe to use with google timeline.
 
@@ -27,34 +30,35 @@ def create_gtimeline_dict(df, row_label, bar_label, start, end, tooltip):
 
     def create_header(row_label, bar_label, start_label, end_label):
         heading = [
-            {"type":"string","id":row_label},
-            {"type":"string","id":bar_label},
-            {"type":"string","role":"tooltip","p":
-                {"html":"true"}
-            },
-            {"type":"date","id":start_label},
-            {"type":"date","id":end_label}
+            {"type": "string", "id": row_label},
+            {"type": "string", "id": bar_label},
+            {"type": "string", "role": "tooltip", "p":
+                {"html": "true"}
+             },
+            {"type": "date", "id": start_label},
+            {"type": "date", "id": end_label}
         ]
         return heading
 
     def create_row(row_label, bar_label, tooltip, start_ts, end_ts):
-        start_ts_str =create_date_str(start_ts)
+        start_ts_str = create_date_str(start_ts)
         end_ts_str = create_date_str(end_ts)
-        row = {"c":[
-                {"v":str(row_label)},
-                {"v":str(bar_label)},
-                {"v":tooltip},
-                {"v":start_ts_str},
-                {"v":end_ts_str}
-                ]}
+        row = {"c": [
+            {"v": str(row_label)},
+            {"v": str(bar_label)},
+            {"v": tooltip},
+            {"v": start_ts_str},
+            {"v": end_ts_str}
+        ]}
         return row
 
     # initialize base structure
     data = dict(cols=None, rows=[])
-    data['cols']=create_header('cam_id', 'interval_id', 'start_time', 'end_time')
+    data['cols'] = create_header('cam_id', 'interval_id', 'start_time', 'end_time')
     for i, row in enumerate(df.iterrows()):
         row = row[1]
-        row_to_ins = create_row(str(row[row_label]), str(row[bar_label]), row[tooltip], row[start], row[end])
+        row_to_ins = create_row(str(row[row_label]), str(
+            row[bar_label]), row[tooltip], row[start], row[end])
         data['rows'].append(row_to_ins)
 
     return data
